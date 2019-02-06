3eb9a5bf846b41307a4d05d40dfbd8fb9bba0a87
adisr21 <sukarnorachman@gmail.com>
2019-02-06 13:54:15 +0700

    add page riwayat, latihan dan pelajaran

---
+++ spiki/spiki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spiki
+//
+//  Created by Adi Sukarno Rachman on 30/01/19.
+//  Copyright © 2019 Adi Sukarno Rachman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
